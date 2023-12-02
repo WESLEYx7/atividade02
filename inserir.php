66abe9afefa2612620546f33ab46c841396e67c5
-f <-f>
2023-12-01 22:42:07 -0400

    atualizandoo

--- inserir.php
+++ inserir.php
@@ -1,34 +1,43 @@
-<?php 
+<?php
+// Dados da conta de usuário do MySQL para ser passado como parâmetro
+$servidor = "localhost";
+$usuario = "aluno";
+$senha = "123";
+$banco = "db_web";
 
-    //Dados da conta de usuário do MySQl para ser passado como parâmetro
-    $servidor = "localhost";
-    $usuario = "aluno";
-    $senha = "123";
-    $banco = "db_web";
+// Try para a tentativa de criação da tabela e inserção dos dados
+try {
+    $conn = new PDO("mysql:host=$servidor;dbname=$banco", $usuario, $senha);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // Comando SQL para inserir valores na tabela 'usuarios' (apenas para teste)
     
-    //Try para a tentativa de criação da tabela e insersão dos dados
-    try {
-        $conn = new PDO("mysql:host=$servidor;dbname=$banco", $usuario, $senha);
-        //echo "Deuuu cerrtoooo KARAIIII";
-        //Configura erros
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        // $sql = "INSERT INTO  usuarios(nome, sobrenome, email) VALUES 
+        //     ('SAMUEL', 'WESLEY', 'borrachinha123@email.com')";
+        // $conn->exec($sql);
+        // echo "Registro inserido com sucesso!";
 
-        //Comando SQL para inserir os valores para a tabela
-        $sql = 
-            "INSERT INTO usuarios(nome,sobrenome,email) VALUES (
-                'SAMUEL', 'WESLEY', 'borrachinha123@email.com'
-            );
-        ";
 
-        //Executar o comando SQL
-        $conn->exec($sql);
-        //echo "Comando executado com sucesso";
+    // Verifica se o formulário foi enviado
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-        //Capturar exception de erro do PDO ou comandos sql no try
-    } catch (PDOException $e) {
-        echo "Error..." . $e->getMessage();
+        // Obter dados do formulário para 'usuarios'
+        $nome = $_POST['nome'];
+        $sobrenome = $_POST['sobrenome'];
+        $email = $_POST['email'];
+
+        // Inserir dados na tabela 'usuarios'
+        $stmtUsuarios = $conn->prepare("INSERT INTO usuarios (nome, sobrenome, email) VALUES (?, ?, ?)");
+        $stmtUsuarios->execute([$nome, $sobrenome, $email]);
     }
 
+    // Recuperar dados da tabela 'usuarios'
+    $stmtUsuarios = $conn->query("SELECT * FROM usuarios");
+    $resultadosUsuarios = $stmtUsuarios->fetchAll(PDO::FETCH_ASSOC);
+
+} catch (PDOException $e) {
+    echo "Error..." . $e->getMessage();
+}
 ?>
 
 <!DOCTYPE html>
@@ -56,9 +65,11 @@
 
     <div class="divPrincipal">
         <form action="./inserir.php" method="post">
-        <h3>Insira seus dados</h3>
-            <input placeholder="Digite seu nome" type="text">
-            <input placeholder="Digite seu nome" type="text">
+            <h3>Insira seus dados</h3>
+                <input name="nome" placeholder="Digite seu nome" type="text">
+                <input name="sobrenome" placeholder="Digite seu sobrenome" type="text">
+                <input name="email" placeholder="Digite seu email" type="text">
+                <input type="submit" value="INSERIR">
         </form>
     </div>
     
--- recuperar_dado.php
+++ recuperar_dado.php
@@ -1,39 +1,53 @@
-<?php 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Listar Dados</title>
+    <link rel="stylesheet" href="./styles/styleInserir.css">
+</head>
+<body>
+    <header>
+        <div class="ladoEsquerdo">
+            <h1>Inserir Dados</h1>
+        </div>
+        
+        <!-- Links para as páginas de cada etapa -->
+        <div class="links">
+            <a href="./index.php">Home</a>
+            <a onclick="aviso()" id="criarTabela" href="./criar_tabela.php">Criar Tabela</a>
+            <a href="./inserir.php">Inserir Dados</a>
+            <a href="recuperar_dado.php">Listar Dados</a>
+        </div>
+    </header>
 
-    //Dados da conta de usuário do MySQl para ser passado como parâmetro
+    <?php
+    // Dados da conta de usuário do MySQL para serem passados como parâmetro
     $servidor = "localhost";
     $usuario = "aluno";
     $senha = "123";
     $banco = "db_web";
-    
+
     try {
         $conn = new PDO("mysql:host=$servidor;dbname=$banco", $usuario, $senha);
-        //echo "Deuuu cerrtoooo";
-
-        //Configurar erros
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        //Comando SQL para listar o nome e o sobrenome dos usuários
-        $sql = "
-           SELECT nome, sobrenome FROM usuarios
-        ";
+        // Comando SQL para listar o nome, sobrenome e email dos usuários
+        $sql = "SELECT nome, sobrenome, email FROM usuarios";
 
-        //Passando para a variável $dado a busca com a query fetchAll que serve para buscar todos
+        // Passando para a variável $dado a busca com a query fetchAll que serve para buscar todos
         $dado = $conn->query($sql)->fetchAll();
 
-        //Laço de repetição para jogar na tela os usuários com seus respectivos dados
+        // Laço de repetição para exibir na tela os usuários com seus respectivos dados
         foreach($dado as $item) {
             echo "Nome: {$item['nome']},
-            Sobrenome: {$item['sobrenome']} <br>";
+            Sobrenome: {$item['sobrenome']} 
+            Email: {$item['email']} <br>";
         }
-
-        //Executar o comando SQL
-        $conn->exec($sql);
-        echo "Comando executado com sucesso";
-
-        //Capturar exception de erro do PDO ou comandos sql no try
     } catch (PDOException $e) {
         echo "Error..." . $e->getMessage();
     }
 
-?>
+    ?>
+</body>
+</html>
